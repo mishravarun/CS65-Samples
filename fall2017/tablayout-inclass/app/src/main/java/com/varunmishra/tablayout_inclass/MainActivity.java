107efbaef50db1c14ebe1a60084e94164d2092f2
Varun Mishra <varun@varunmishra.com>
2017-10-11 12:57:34 -0400

    updated

--- fall2017/tablayout-inclass/app/src/main/java/com/varunmishra/tablayout_inclass/MainActivity.java
+++ fall2017/tablayout-inclass/app/src/main/java/com/varunmishra/tablayout_inclass/MainActivity.java
@@ -1,15 +1,21 @@
 package com.varunmishra.tablayout_inclass;
 
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.varunmishra.tablayout_inclass.view.SlidingTabLayout;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    public TextView tv;
 //Changes
     private SlidingTabLayout slidingTabLayout;
     private ViewPager mViewPager;
@@ -30,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         fragments.add(new MeetFragment());
         fragments.add(new PartyFragment());
 
-        mViewPagerAdapter = new TabViewPagerAdapter(getSupportFragmentManager(),fragments);
+        mViewPagerAdapter = new TabViewPagerAdapter(getFragmentManager(),fragments);
 
         mViewPager.setAdapter(mViewPagerAdapter);
 
@@ -38,5 +44,9 @@ public class MainActivity extends AppCompatActivity {
         slidingTabLayout.setViewPager(mViewPager);
 
     }
-
+    public void testClick(View v){
+        tv=findViewById(R.id.summary);
+        tv.setText("text");
+        Log.d("Test","tt");
+    }
 }
--- fall2017/tablayout-inclass/app/src/main/java/com/varunmishra/tablayout_inclass/PartyFragment.java
+++ fall2017/tablayout-inclass/app/src/main/java/com/varunmishra/tablayout_inclass/PartyFragment.java
@@ -1,18 +1,50 @@
 package com.varunmishra.tablayout_inclass;
 
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class PartyFragment extends Fragment {
-	
+public class PartyFragment extends PreferenceFragment {
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.preference);
+
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.partyfragment, container, false);
+        Log.d("TAGG", "came here");
+        View view = super.onCreateView(inflater, container, savedInstanceState);
+
+        return view;
     }
-    
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+    }
 }
 
